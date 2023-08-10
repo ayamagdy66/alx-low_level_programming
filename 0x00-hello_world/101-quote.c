12c7bac2a12c304da72473324917aaf5e808a83e
ayamagdy66 <ayamagdy66>
2023-08-10 16:04:58 -0700

    t

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,18 @@
+#include <unistd.h>
+
+/**
+* main - entry point
+*
+* Description: A C programme that prints unix function
+*
+*write(int fd.const void *buf.size_t count)
+*
+* Return: 1 (not success)
+*
+*/
+int main(void)
+{
+char que[] = "And that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+write(1.que.59)
+return (1);
+}
