66532befe9001af6aba7aadd841eb89ee34c49dc
ayamagdy66 <ayamagdy20120@gmail.com>
2023-08-14 23:35:58 -0700

    t

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main _ entry point
+ *
+ * Description: A C programme writ a script fot _putchar
+ *
+ * Return: 0 (Success)
+ *
+*/
+int main(void)
+{
+printf("_putchar\n");
+
+return (0);
+}
