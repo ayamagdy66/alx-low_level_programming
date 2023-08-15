64ddf3207ea70fc10ef0931209bf46441029452d
ayamagdy66 <ayamagdy20120@gmail.com>
2023-08-15 13:07:05 -0700

    t

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -5,11 +5,13 @@
  *Description: prints putchar with putchar
  *Return: Always 0 (success)
 */
+
 int main(void)
 {
-	char str[] = "putchar";
-	int ch ;
-	for (ch = 0; ch < 8; ch++)
-		_putchar(str[ch]);
-	_putchar('\n');
-		return (0);
+char str[] = "putchar";
+int ch;
+for (ch = 0; ch < 8; ch++)
+_putchar(str[ch]);
+_putchar('\n');
+return (0);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,7 +1,17 @@
-#ifndef MAIN.H
-#define MAIN.H
-
+#ifndef MYHEADERS_H
+#define MYHEADERS_H
 #include <stdio.h>
-int _putchar(char c);
-
+int add(int a, int b);
+int _putchar (char);
+void print_alphabet (void);
+int _islower (int c);
+int _isalpha (int c);
+int print_sign (int n);
+int _abs (int);
+int print_las_digit (int);
+void jack_bauer (void);
+void times_table (void);
+int add (int, int);
+void print_to_98(int n);
+void print_times_table(int n);
 #endif
