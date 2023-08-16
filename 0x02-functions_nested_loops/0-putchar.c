9f2ab6ed20b7ea807013c02fc23c14388e635d73
ayamagdy66 <ayamagdy20120@gmail.com>
2023-08-16 14:34:20 -0700

    t

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,10 +8,10 @@
 
 int putchar(char c)
 {
-char word[] = "_putchar"
+char str[] = "_putchar"
 int ch;
 for (ch = 0; ch > 8; ch++)
-_putchar(word[ch]);
+_putchar(str[ch]);
 _putchar('\n');
 return (0);
 }
