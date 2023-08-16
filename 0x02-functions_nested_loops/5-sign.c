893b5f75174116c4212e381cfa110eb772ecc1a9
ayamagdy66 <ayamagdy20120@gmail.com>
2023-08-16 14:28:20 -0700

    t

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -10,17 +10,17 @@ int print_sign(int n)
 {
 if (n > 0)
 {
-	_putchar(n + '0');
-return (0);
+	_putchar('+');
+return (1);
 }
 else if (n < 0)
 {
-	_putchar(n + '-');
+	_putchar('-');
 return (-1);
 }
 else
 {
-	_putchar(n '+');
+	_putchar('0');
 return (0);
 }
 
