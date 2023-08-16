56cfa981fb5814e7e8a48cada6e8f889b7a23530
ayamagdy66 <ayamagdy20120@gmail.com>
2023-08-16 13:51:38 -0700

    t

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_sign - prints the sign of the given number +/-
+ * @n: Test the number
+ * Return: 1 if zero else return (-1) < n, or (+) > zero
+ *
+*/
+int print_sign(int n)
+
+{
+if (n > 0)
+{
+	_putchar('+');
+return (0);
+}
+else if (n < 0)
+{
+	_putchar('-');
+return (-1);
+}
+else if (n == 0)
+{
+	_putchar('0')
+return (0);
+}
+
+}
